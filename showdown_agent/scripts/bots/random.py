ba0e60192307d59ce4cfa363ff32d07bab717ba2
Suppise152 <tcas362@aucklanduni.ac.nz>
2025-07-25 14:57:32 +1200

    set up

--- showdown_agent/scripts/bots/random.py
+++
@@ -1,6 +0,0 @@
-from poke_env import RandomPlayer
-
-
-class CustomAgent(RandomPlayer):
-    def __init__(self, team, *args, **kwargs):
-        super().__init__(team=team, *args, **kwargs)
--- showdown_agent/scripts/bots/simple.py
+++
@@ -1,6 +0,0 @@
-from poke_env import SimpleHeuristicsPlayer
-
-
-class CustomAgent(SimpleHeuristicsPlayer):
-    def __init__(self, team, *args, **kwargs):
-        super().__init__(team=team, *args, **kwargs)
